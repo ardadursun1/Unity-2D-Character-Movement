688031e01cc5c0da0865cc83400b169a2056f5f3
ardadursun1 <72658090+ardadursun1@users.noreply.github.com>
2021-05-31 19:46:24 +0300

    Add files via upload

---
+++ CharacterMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask platformLayerMask;
+
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
+
+    private void Awake() {
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+    }
+
+    private void Update() {
+        // Handle Jump
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space)) {
+            float jumpVelocity = 7f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+    private void FixedUpdate() {
+        // Handle Movement
+        float moveSpeed = 10f;
+        rigidbody2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+        if (Input.GetKey(KeyCode.A)) {
+            rigidbody2d.velocity = new Vector2(-moveSpeed, rigidbody2d.velocity.y);
+        } else {
+            if (Input.GetKey(KeyCode.D)) {
+                rigidbody2d.velocity = new Vector2(+moveSpeed, rigidbody2d.velocity.y);
+            } else {
+                // No keys pressed
+                rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+                rigidbody2d.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            }
+        }
+    }
+
+    private bool IsGrounded() {
+        //return transform.Find("GroundCheck").GetComponent<GroundCheck>().isGrounded;
+
+        float extraHeightText = 1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, extraHeightText, platformLayerMask);
+
+        Color rayColor;
+        if (raycastHit.collider != null) {
+            rayColor = Color.green;
+        } else {
+            rayColor = Color.red;
+        }
+        
+        return raycastHit.collider != null;
+    }
+}
