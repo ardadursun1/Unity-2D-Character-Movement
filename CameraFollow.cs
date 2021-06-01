71bc650532ff176381d8f061908ee43bae52a7e4
ardadursun1 <72658090+ardadursun1@users.noreply.github.com>
2021-06-02 00:20:37 +0300

    Update CameraFollow.cs

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -7,9 +7,6 @@ public class CameraFollow : MonoBehaviour
     public Transform player;
     public Vector3 offset;
 
-    /*private void Awake() {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-    }*/    
     private void FixedUpdate () {
       transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
     }
